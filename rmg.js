5c7a7b3d61adb8919ab41b36a2c2cb3cee42c3be
Charlie Ballentine <whereischarlie@gmail.com>
2024-04-19 15:44:55 +1200

    Added some subjects, actions & settings for hilarious message combinations

--- rmg.js
+++ rmg.js
@@ -1,1 +1,3 @@
-console.log("Hello World");
+const subjects = ['Ducks', 'Frogs', 'Camels', 'Cats', 'Hobos', 'Trees'];
+const actions = ['whispering', 'farting', 'tripping', 'jumping', 'falling'];
+const settings = ['where the crawdads sing', 'in the wasteland', 'into the wilds', 'behind the shed', 'on your bed'];
