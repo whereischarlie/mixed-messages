8690e0778264540067d0d0f903457e5341b07127
Charlie Ballentine <whereischarlie@gmail.com>
2024-04-19 15:50:58 +1200

    Added code to get random pieces of the message

--- rmg.js
+++ rmg.js
@@ -1,3 +1,15 @@
 const subjects = ['Ducks', 'Frogs', 'Camels', 'Cats', 'Hobos', 'Trees'];
 const actions = ['whispering', 'farting', 'tripping', 'jumping', 'falling'];
 const settings = ['where the crawdads sing', 'in the wasteland', 'into the wilds', 'behind the shed', 'on your bed'];
+
+const getRandomSubject = () => {
+  return subjects[Math.floor(Math.random() * subjects.length)];
+};
+
+const getRandomAction = () => {
+  return actions[Math.floor(Math.random() * actions.length)];
+};
+
+const getRandomSetting = () => {
+  return settings[Math.floor(Math.random() * settings.length)];
+};
