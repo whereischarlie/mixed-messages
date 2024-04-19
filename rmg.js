01253db4b65dba50e4f51dbcac8e9d814fbe1b38
Charlie Ballentine <whereischarlie@gmail.com>
2024-04-19 16:10:39 +1200

    Refactor code to remove code duplication

--- rmg.js
+++ rmg.js
@@ -2,27 +2,19 @@ const subjects = ['ducks', 'frogs', 'camels', 'cats', 'hobos', 'trees'];
 const actions = ['whispering to', 'farting on', 'tripping over', 'jumping into', 'falling beside'];
 const settings = ['where the crawdads sing', 'in the wasteland', 'in the remote wilds', 'behind the shed', 'on your bed'];
 
-const getRandomSubject = () => {
-  return subjects[Math.floor(Math.random() * subjects.length)];
-};
-
-const getRandomAction = () => {
-  return actions[Math.floor(Math.random() * actions.length)];
-};
-
-const getRandomSetting = () => {
-  return settings[Math.floor(Math.random() * settings.length)];
-};
+const getRandomItem = (items) => {
+  return items[Math.floor(Math.random() * items.length)];
+}
 
 const getRandomMessage = () => {
   let randomMessageSelector = Math.floor(Math.random() * 3);
   switch (randomMessageSelector) {
     case 0:
-      return 'did you hear the one about the ' + getRandomSubject() + ' ' + getRandomAction() + ' the ' + getRandomSubject() + ' ' + getRandomSetting() + '?';
+      return 'did you hear the one about the ' + getRandomItem(subjects) + ' ' + getRandomItem(actions) + ' the ' + getRandomItem(subjects) + ' ' + getRandomItem(settings) + '?';
     case 1:
-      return getRandomSubject() + ' ' + getRandomAction() + ' ' + getRandomSubject() + ' ' + getRandomSetting() + '.';
+      return getRandomItem(subjects) + ' ' + getRandomItem(actions) + ' ' + getRandomItem(subjects) + ' ' + getRandomItem(settings) + '.';
     case 2:
-      return getRandomSetting() + ' there are ' + getRandomSubject() + ' ' + getRandomAction() + ' things in the dark.';
+      return getRandomItem(settings) + ' there are ' + getRandomItem(subjects) + ' ' + getRandomItem(actions) + ' things in the dark.';
   }
 };
 
