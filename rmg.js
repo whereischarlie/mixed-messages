bfcbb90e5d020b270aa196d39b54d6c948df0e8a
Charlie Ballentine <whereischarlie@gmail.com>
2024-04-19 16:00:58 +1200

    Tweaked the message components & added the message generator.

--- rmg.js
+++ rmg.js
@@ -1,6 +1,6 @@
-const subjects = ['Ducks', 'Frogs', 'Camels', 'Cats', 'Hobos', 'Trees'];
-const actions = ['whispering', 'farting', 'tripping', 'jumping', 'falling'];
-const settings = ['where the crawdads sing', 'in the wasteland', 'into the wilds', 'behind the shed', 'on your bed'];
+const subjects = ['ducks', 'frogs', 'camels', 'cats', 'hobos', 'trees'];
+const actions = ['whispering to', 'farting on', 'tripping over', 'jumping into', 'falling beside'];
+const settings = ['where the crawdads sing', 'in the wasteland', 'in the remote wilds', 'behind the shed', 'on your bed'];
 
 const getRandomSubject = () => {
   return subjects[Math.floor(Math.random() * subjects.length)];
@@ -13,3 +13,17 @@ const getRandomAction = () => {
 const getRandomSetting = () => {
   return settings[Math.floor(Math.random() * settings.length)];
 };
+
+const getRandomMessage = () => {
+  let randomMessageSelector = Math.floor(Math.random() * 3);
+  switch (randomMessageSelector) {
+    case 0:
+      return 'did you hear the one about the ' + getRandomSubject() + ' ' + getRandomAction() + ' the ' + getRandomSubject() + ' ' + getRandomSetting() + '?';
+    case 1:
+      return getRandomSubject() + ' ' + getRandomAction() + ' ' + getRandomSubject() + ' ' + getRandomSetting() + '.';
+    case 2:
+      return getRandomSetting() + ' there are ' + getRandomSubject() + ' ' + getRandomAction() + ' things in the dark.';
+  }
+};
+
+console.log(getRandomMessage());
